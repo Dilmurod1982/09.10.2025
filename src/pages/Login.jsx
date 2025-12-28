1ef019a8e477f543f52e55104ee06a3fb3ee9b57
Dilmurod1982 <dilsurbet@gmail.com>
2025-12-28 22:15:52 +0300

    4564

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -163,7 +163,7 @@ function Login() {
                     </h1>
                   </div>
                   <h2 className="text-3xl font-bold text-gray-800 mb-2">
-                    Тизимга кириш
+                    Тизимга киришни
                   </h2>
                   <p className="text-gray-600">
                     Давом этиш учун қаторларни тўлдиринг
